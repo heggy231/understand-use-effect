c4e45fdc5f7106554aa49bfd55a5b8dbc39e4b7a
Heggy Castaneda <heggyy@gmail.com>
2021-07-25 10:19:10 -0400

    review dependencies useEffect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
 
   const [counter, setCounter] = useState(99);
   const [posts, setPosts] = useState([]);
+  const [name, setName] = useState('');
+
+  useEffect( () => {
+    console.log('useEffect');
+  }, [counter])
 
   useEffect(() => {
 
@@ -14,10 +19,11 @@ function App() {
     .then(resultJson => {
       setPosts(resultJson);
     })
-    
+
   }, [])
 
   const handleIncrement = () => setCounter(counter + 1);
+  const handleChangeNameClick = () => setName('Heggy')
 
   const postItem = posts.map(post => {
     return (
@@ -32,6 +38,8 @@ function App() {
     <div className="App">
       <h1>{counter}</h1>
       <button onClick={handleIncrement}>+</button>
+      <h2>Name: {name}</h2>
+      <button onClick={handleChangeNameClick}>Change Name</button>
       <h2>Posts</h2>
       <ul>
         {
